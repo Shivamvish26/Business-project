43d23618d4caa557f4baf81f003c3095aa11ba5b
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2026-08-16 12:02:38 +0530

    gallery api done started with booking api

--- furniture-polish-backend/controllers/galleryController.js
+++ furniture-polish-backend/controllers/galleryController.js
@@ -1,5 +1,6 @@
 const Gallery = require("../models/Gallery");
 
+// create service api
 const createGallery = async (req, res) => {
   try {
     const { title, category, description, service, status } = req.body;
@@ -38,6 +39,119 @@ const createGallery = async (req, res) => {
   }
 };
 
+// get all service api
+const getAllGallery = async (req, res) => {
+  try {
+    const gallery = await Gallery.find().sort({ createdAt: -1 });
+    res.status(200).json({
+      message: "Gallery fetched successfully",
+      gallery: gallery,
+    });
+  } catch (error) {
+    console.log("Faild to get all services", error);
+    res.status(500).json({
+      message: "Faild to feth the gallery",
+      error: message.error,
+    });
+  }
+};
+
+// update gallery api
+const updateGallery = async (req, res) => {
+  try {
+    const gallery = await Gallery.findById(req.params.id);
+
+    if (!gallery) {
+      return res.status(404).json({
+        message: "Gallery Not Found",
+      });
+    }
+
+    const updateData = {
+      title: req.body.title,
+      category: req.body.category,
+      description: req.body.description,
+      service: req.body.service,
+      status: req.body.status,
+    };
+    if (req.files?.beforeImage) {
+      updateData.beforeImage = `/uploads/gallery/${req.files.beforeImage[0].filename}`;
+    }
+    if (req.files?.afterImage) {
+      updateData.afterImage = `/uploads/gallery/${req.files.afterImage[0].filename}`;
+    }
+
+    const updatedGallery = await Gallery.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    res.status(200).json({
+      message: "Gallery Updated Successfully",
+      gallery: updatedGallery,
+    });
+  } catch (error) {
+    console.log("Failed to Update Gallery:", error);
+
+    res.status(500).json({
+      message: "Failed to Update Gallery",
+      error: error.message,
+    });
+  }
+};
+
+// delet api
+const deletegallery = async (req, res) => {
+  try {
+    const gallery = await Gallery.findByIdAndDelete(req.params.id);
+    if (!gallery) {
+      return res.status(404).json({
+        message: "Gallery Not Found",
+      });
+    }
+    res.status(200).json({
+      message: "Gallery Deleted Sucessfully",
+      gallery,
+    });
+  } catch (error) {
+    console.log("Failed to delete the gallery", error);
+    res.status(500).json({
+      message: "Faild to delete the galley",
+      error: error.message,
+    });
+  }
+};
+
+// single Gallery api
+const getsingleGallery = async (req, res) => {
+  try {
+    const gallery = await Gallery.findById(req.params.id);
+    if (!gallery) {
+      return res.status(404).json({
+        message: "Single Gallery post not found",
+      });
+    }
+    res.status(200).json({
+      message: "Single Gallery Fetched",
+      gallery,
+    });
+  } catch (error) {
+    console.log("Failed to fetch the single post");
+    res.status(500).json({
+      message: "Faild to fetch the Gallery",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   createGallery,
+  getAllGallery,
+  updateGallery,
+  deletegallery,
+  getsingleGallery
 };
--- furniture-polish-backend/routes/galleryRoutes.js
+++ furniture-polish-backend/routes/galleryRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { createGallery } = require("../controllers/galleryController");
+const {
+  createGallery,
+  getAllGallery,
+  updateGallery,
+  deletegallery,
+  getsingleGallery,
+} = require("../controllers/galleryController");
 
 const upload = require("../middleware/uploadMiddleware");
 
+// create post api
 router.post(
   "/create-gallery",
   upload.fields([
@@ -12,6 +19,35 @@ router.post(
     { name: "afterImage", maxCount: 1 },
   ]),
   createGallery,
+);
+
+// get gallery api
+router.get("/get-gallery", getAllGallery);
+
+// update gallery api
+router.put(
+  "/:id",
+  upload.fields([
+    { name: "beforeImage", maxCount: 1 },
+    { name: "afterImage", maxCount: 1 },
+  ]),
+  updateGallery,
+);
+router.delete(
+  "/:id",
+  upload.fields([
+    { name: "beforeImage", maxCount: 1 },
+    { name: "afterImage", maxCount: 1 },
+  ]),
+  deletegallery,
+);
+router.get(
+  "/:id",
+  upload.fields([
+    { name: "beforeImage", maxCount: 1 },
+    { name: "afterImage", maxCount: 1 },
+  ]),
+  getsingleGallery,
 );
 
 module.exports = router;
